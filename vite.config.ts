eacb5e728f3cb28458640cc6cc278d9118bda499
AuryChar <nat4nz007love@gmail.com>
2025-11-07 13:39:14 -0300

    allow ryterm.xyz

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,10 @@ const PORT = 3000
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
 	server: {
-		port: PORT,
-		allowedHosts: ["d2fb15995299.ngrok-free.app"]
+		port: PORT
 	},
 	preview: {
-		port: PORT
+		port: PORT,
+		allowedHosts: ["ryterm.xyz"]
 	}
 });
