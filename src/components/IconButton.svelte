842f2d4b0f857e89bdcc6610932a10ada958c7c2
AuryChar <nat4nz007love@gmail.com>
2025-11-09 23:24:12 -0300

    Add transition effect

--- src/components/IconButton.svelte
+++ src/components/IconButton.svelte
@@ -5,7 +5,7 @@
 <button
 	title={label}
 	onclick={func}
-	class="cursor-pointer flex justify-center p-2 rounded-xl items-center border hover:bg-neutral-200 dark:hover:bg-neutral-800 border-neutral-300 dark:border-neutral-700"
+	class="transition-all cursor-pointer flex justify-center p-2 rounded-xl items-center border hover:bg-neutral-200 dark:hover:bg-neutral-800 border-neutral-300 dark:border-neutral-700"
 >
 	<i class={`bxr  bx-${icon}`}></i>
 </button>
--- src/components/SecondaryButton.svelte
+++ src/components/SecondaryButton.svelte
@@ -3,10 +3,12 @@
 </script>
 
 <button
-	class="cursor-pointer select-none flex justify-center items-center border rounded-xl p-2 bg-white dark:bg-neutral-950 border-neutral-300 hover:bg-neutral-200 dark:border-neutral-700 hover:dark:bg-neutral-800"
+	class="transition-all cursor-pointer select-none flex justify-center items-center border rounded-xl p-2 bg-white dark:bg-neutral-950 border-neutral-300 hover:bg-neutral-200 dark:border-neutral-700 hover:dark:bg-neutral-800"
 	onclick={func}
 >
-	{label}
+	{
+		label
+	}
 </button>
 
 <style>
