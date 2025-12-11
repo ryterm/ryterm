768b4f7b6e2264757b307308c7ea3d7ecf92541a
Aury Vaz <aury@ryterm.xyz>
2025-12-11 00:10:18 -0300

    remove tailwindcss
    Signed-off-by: Aury Vaz <aury@ryterm.xyz>

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import tailwindcss from "@tailwindcss/vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
 const PORT = 3000;
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
+	plugins: [sveltekit()],
 	server: {
 		port: PORT,
 	},
