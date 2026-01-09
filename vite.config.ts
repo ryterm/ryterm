363a0d583f1b950fc587d9599eee2f70c748cc90
Aury Vaz <aury@ryterm.xyz>
2026-01-09 18:46:09 -0300

    change port and allowed host
    Signed-off-by: Aury Vaz <aury@ryterm.xyz>

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,15 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const PORT = 3000;
+
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()]
+    plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+    server: {
+        port: PORT,
+    },
+    preview: {
+        port: PORT,
+        allowedHosts: ["ryterm.xyz"],
+    },
 });
